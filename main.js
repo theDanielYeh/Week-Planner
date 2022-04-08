2c9d844a8e29de502a8c564927f9e24f15893e9e
Derek Lui <luiderek@outlook.com>
2022-04-08 17:53:59 +0000

    first pass some css

---
+++ main.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+const $day_of_week_list = document.querySelectorAll('.day-of-week');
+const $schedule_events_header = document.querySelector('.schedule-events-header');
+const $modal_add = document.querySelector('add-entry-modal');
+const $modal_edit = document.querySelector('edit-entry-modal');
+const $modal_delete = document.querySelector('delete-entry-modal');
+
+const $add_entry = document.querySelector('.add-entry');
